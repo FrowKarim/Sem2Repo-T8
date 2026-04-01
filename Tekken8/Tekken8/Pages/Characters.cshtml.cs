48de4f1d22d66feb2f410d1ca0c8efd6ad34f847
FrowKarim <57719623+FrowKarim@users.noreply.github.com>
2026-04-01 11:34:34 +0200

    Character page added
    Character page added with mr jippity

--- Tekken8/Tekken8/Pages/Characters.cshtml.cs
+++ Tekken8/Tekken8/Pages/Characters.cshtml.cs
@@ -1,13 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Tekken8.Pages
 {
     public class CharactersModel : PageModel
-    {
+    {               
+        public List<string> ImagePaths { get; set; }
+
         public void OnGet()
         {
+            var imagesPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "images");
+
+            if (Directory.Exists(imagesPath))
+            {
+                ImagePaths = Directory.GetFiles(imagesPath)
+                    .Where(f => IsImageFile(f))
+                    .Select(f => "/images/" + Path.GetFileName(f))
+                    .ToList();
+            }
+            else
+            {
+                ImagePaths = new List<string>();
+            }
+        }
+
+        private bool IsImageFile(string filePath)
+        {
+            var imageExtensions = new[] { ".jpg", ".jpeg", ".png", ".gif", ".webp" };
+            var extension = Path.GetExtension(filePath).ToLower();
+            return imageExtensions.Contains(extension);
         }
     }
 }
-
