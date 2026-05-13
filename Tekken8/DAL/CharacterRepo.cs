d20c21fd76e9c43c9a09df610980776e79ffdba7
FrowKarim <57719623+FrowKarim@users.noreply.github.com>
2026-05-13 12:09:24 +0200

    Add DB access to CharacterRepo and SQL client
    Implemented database reading in DAL/CharacterRepo.cs: GetAllCharacters and GetCharacterById now open a SQL connection, execute queries against CharacterFrameData, and map reader fields (Id, characterName, editUrl, game) to Character objects. Added Microsoft.Data.SqlClient package reference to DAL.csproj. Added an empty internal CharacterService class in LogicLayer and updated the solution file to include the Tekken8Tests project. Note: connection string is inlined in the repo files; consider moving it to configuration and parameterizing queries in a follow-up.

--- Tekken8/DAL/CharacterRepo.cs
+++ Tekken8/DAL/CharacterRepo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using LogicLayer;
+using Microsoft.Data.SqlClient;
 
 namespace DAL
 {
@@ -10,18 +11,64 @@ namespace DAL
         {
             List<Character> CharacterList = new List <Character>();
 
+            string connectionString = ("Server=mssqlstud.fhict.local;" +
+                                "Database=dbi439179_tekken;" +
+                                "User Id=dbi439179_tekken;" +
+                                "Password=TKDB; " +
+                                "TrustServerCertificate = true");
 
-            //CharacterList.Add()
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand sqlcommand = new SqlCommand("SELECT * FROM CharacterFrameData ", connection))
+                {
 
+                    using (SqlDataReader reader = sqlcommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Character character = new Character();
+                            character.Id = Convert.ToInt32(reader["Id"]);
+                            character.Name = reader["characterName"].ToString();
+                            character.EditUrl = reader["editUrl"].ToString();
+                            character.Game = reader["game"].ToString();
 
+                            CharacterList.Add(character);
+                        }
+                    }
+                }
+            }
             return CharacterList;
-            
-            throw new NotImplementedException();
         }
 
         public Character GetCharacterById(int id)
         {
-            throw new NotImplementedException();
+            Character character = new Character();
+            string connectionString = ("Server=mssqlstud.fhict.local;" +
+                                "Database=dbi439179_tekken;" + 
+                                "User Id=dbi439179_tekken;" +
+                                "Password=TKDB; " +
+                                "TrustServerCertificate = true");
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                using (SqlCommand sqlcommand = new SqlCommand("SELECT * FROM CharacterFrameData WHERE Id = @Id"))
+                {
+                    using (SqlDataReader reader = sqlcommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            character.Id = Convert.ToInt32(reader["Id"]);
+                            character.Name = reader["characterName"].ToString();
+                            character.EditUrl = reader["editUrl"].ToString();
+                            character.Game = reader["game"].ToString();
+
+                        }
+                    }
+                }
+            }
+            return character;
         }
 
         public List<Move> GetCharacterMovesById(int id)
---
+++ Tekken8/LogicLayer/CharacterService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLayer
+{
+    public class CharacterService
+    {
+        private readonly ICharacterRepo _characterRepo;
+
+        public CharacterService(ICharacterRepo characterRepo)
+        {
+            _characterRepo = characterRepo;
+        }
+
+        public List<Character> GetAllCharacters()
+        {
+            return _characterRepo.GetAllCharacters();
+        }
+
+        public Character GetCharacterById(int characterId)
+        {
+            return _characterRepo.GetCharacterById(characterId);
+        }
+
+        public List<Move> GetCharacterMovesById(int characterId)
+        {
+            return _characterRepo.GetCharacterMovesById(characterId);
+        }
+    }
+}
